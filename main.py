b474c219b1c0ec297bbb57e211e7adfbb4fc4b22
amotz taub tabib <amotz.tt@gmail.com>
2021-04-24 13:14:58 +0300

    chaging spellings

--- main.py
+++ main.py
@@ -24,18 +24,18 @@ def main():
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch size', type=int, defult=64, help='batch size for training')
-    parser.add_argument('--dataset_name', type=str, defult='shape_class', help='dataset_name')
-    parser.add_argument('--epochs_number', type=int, defult=64, help='epoch number for training')
-    parser.add_argument('--classes', type=int, defult=3, help='dataset_classes')
-    parser.add_argument('--lr', type=int, defult=0.01, help='learning rate')
+    parser.add_argument('--batch size', type=int, default=64, help='batch size for training')
+    parser.add_argument('--dataset_name', type=str, default='shape_class', help='dataset_name')
+    parser.add_argument('--epochs_number', type=int, default=64, help='epoch number for training')
+    parser.add_argument('--classes', type=int, default=3, help='dataset_classes')
+    parser.add_argument('--lr', type=int, default=0.01, help='learning rate')
     parser.add_argument('--device', type=int, default=0, help='gpu device')  # default is 0
-    parser.add_argument('--weight_decay', type=int, defult=1e-7, help='dropout')
-    parser.add_argument('--cuda', action='store_true', defult=True, help='gpu for training acceleration')
-    parser.add_argument('--model', type=str, defult='resnet50')
-    parser.add_argument('--opt', type=str, defult='adam', choices=('sgd','adam','rmsprop'))
-    parser.add_argument('--root_path', type=str, defult='./data', help='path to dataset')
-    parser.add_argument('--save', type=str, defult='/save/backetnet', help='path to checkpoint save directory')
+    parser.add_argument('--weight_decay', type=int, default=1e-7, help='dropout')
+    parser.add_argument('--cuda', action='store_true', default=True, help='gpu for training acceleration')
+    parser.add_argument('--model', type=str, default='resnet50')
+    parser.add_argument('--opt', type=str, default='adam', choices=('sgd','adam','rmsprop'))
+    parser.add_argument('--root_path', type=str, default='./data', help='path to dataset')
+    parser.add_argument('--save', type=str, default='/save/backetnet', help='path to checkpoint save directory')
 
     args = parser.parse_args()
     return args
