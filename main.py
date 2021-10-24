bcd1a4b7c671b8f3465af97584f7fa52d2a3e2f6
amotz taub tabib <amotz.tt@gmail.com>
2021-10-24 18:25:34 +0300

    changing the position of scheduler.step

--- main.py
+++ main.py
@@ -35,11 +35,11 @@ def main():
     for epoch in range(1, args.epochs_number + 1):
         run_avg.reset_train()
         run_avg.reset_val()
-        scheduler.step()
 
         train(args, model, train_loader, epoch, optimizer, scaler, run_avg)
         val_acc = evaluation(args, model, valid_loader, epoch, run_avg)
 
+        scheduler.step()
         if best_acc < val_acc:
             best_acc = val_acc
             save_checkpoint(model, optimizer, args, epoch)
